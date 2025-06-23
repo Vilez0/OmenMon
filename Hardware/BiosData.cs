cc62dce6b29367378e2606f5a509c55ca6ddccb7
Edip <75365757+Vilez0@users.noreply.github.com>
2025-06-23 18:23:22 +0300

    add a victus standard keyboard type and hope it fixes it for victus :)
    Signed-off-by: Edip <75365757+Vilez0@users.noreply.github.com>

--- Hardware/BiosData.cs
+++ Hardware/BiosData.cs
@@ -91,10 +91,11 @@ namespace OmenMon.Hardware.Bios {
 
         // Keyboard type
         public enum KbdType : byte {
-            Standard   = 0x00,  // Standard layout (or BIOS call failed)
-            WithNumPad = 0x01,  // Standard layout with numerical block
-            TenKeyLess = 0x02,  // Extra navigation keys but no numerical block
-            PerKeyRgb  = 0x03   // Independently-definiable color for each key (?)
+            Standard       = 0x00,  // Standard layout (or BIOS call failed)
+            WithNumPad     = 0x01,  // Standard layout with numerical block
+            TenKeyLess     = 0x02,  // Extra navigation keys but no numerical block
+            PerKeyRgb      = 0x03,  // Independently-definiable color for each key (?)
+            VictusStandard = 0x04   // Victus keyboard
         }
 
         // Keyboard backlight color zone
